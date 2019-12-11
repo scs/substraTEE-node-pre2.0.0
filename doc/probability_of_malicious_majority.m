f99ace2ff3ae14dc7f780689a0352df2abb4dac4
Alain Brenzikofer <news@brenzi.ch>
2019-12-11 18:27:30 +0100

    fixing poisson bug

--- doc/probability_of_malicious_majority.m
+++ doc/probability_of_malicious_majority.m
@@ -1,11 +1,12 @@
-lambda = [0.05 0.1 0.2 0.4 0.6];
+evilrate = [0.05 0.1 0.2 0.4 0.6];
 n = 3:12;
-P=zeros(length(lambda), length(n));
-for lam = 1:length(lambda)
+P=zeros(length(evilrate), length(n));
+for lam = 1:length(evilrate)
   for num = 1: length(n)
     % you yourself are honest, so there will always be at least 1 honest actor
     tmp = [ceil(n(num)/2):n(num)-1];
-    P(lam,num) = exp(-lambda(lam))*sum(lambda(lam).^tmp./factorial(tmp));
+    lambda = evilrate(lam)*n(num);
+    P(lam,num) = exp(-lambda)*sum(lambda.^tmp./factorial(tmp));
   end
 end
 
@@ -13,7 +14,7 @@ semilogy(n, P', 'linewidth', 2)
 ylabel("probability to suffer")
 xlabel("number of registered meetup participants")
 
-hleg= legend(strvcat(num2str(lambda')));
-set(hleg, 'title', 'lambda')
+hleg= legend(strvcat(num2str(evilrate')));
+set(hleg, 'title', 'malicious')
 grid on
 print -dsvg probability_of_malicious_majority.svg
